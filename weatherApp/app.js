ddfca8bc7ada9415fdcd0563bd342fedbb35a342
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-03 18:14:53 +0330

    update js file

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -28,14 +28,35 @@ let bodyImgUrl = document.body
 function randomUrl() {
     let ranUrl = Math.floor(Math.random() * 22)
     bodyImgUrl.style.backgroundImage = 'url(' + imgUrl[ranUrl] + ')'
-    console.log(imgUrl[ranUrl])
 }
 
-let citiesWeather = [
-    {id: 1, cityName: 'Tehran', temp: 35, sky: 'sunny', humidity: 24, wind: 22},
-    {id: 2, cityName: 'Nowshahr', temp: 29, sky: 'rainy', humidity: 34, wind: 34},
-    {id: 3, cityName: 'Qom', temp: 45, sky: 'sunny', humidity: 14, wind: 40},
-    {id: 4, cityName: 'Tabriz', temp: 24, sky: 'rainy', humidity: 22, wind: 29},
-    {id: 5, cityName: 'Shiraz', temp: 39, sky: 'cloudy', humidity: 12, wind: 16},
-    {id: 6, cityName: 'Yazd', temp: 41, sky: 'sunny', humidity: 32, wind: 27},
-]
+let citiesWeather = {
+   tehran: {id: 1, cityName: 'Tehran', temp: 35, sky: 'sunny', humidity: 24, wind: 22},
+   noshahr: {id: 2, cityName: 'Nowshahr', temp: 29, sky: 'rainy', humidity: 34, wind: 34},
+   qom: {id: 3, cityName: 'Qom', temp: 45, sky: 'sunny', humidity: 14, wind: 40},
+   tabriz: {id: 4, cityName: 'Tabriz', temp: 24, sky: 'rainy', humidity: 22, wind: 29},
+   shiraz: {id: 5, cityName: 'Shiraz', temp: 39, sky: 'cloudy', humidity: 12, wind: 16},
+   yazd: {id: 6, cityName: 'Yazd', temp: 41, sky: 'sunny', humidity: 32, wind: 27},
+}
+
+let searchBtn = document.getElementById('btn')
+let searchBar = document.getElementById('user-input')
+
+searchBtn.addEventListener('click', function () {
+    let userInput = searchBar.value.toLowerCase()
+    let tableContent = citiesWeather[userInput]
+
+    if (tableContent) {
+        document.querySelector('.city').innerHTML = tableContent.cityName
+        document.getElementById('temp').innerHTML = tableContent.temp + '°C'
+        document.getElementById('description').innerHTML = tableContent.sky
+        document.getElementById('humidity').innerHTML = 'Humidity ' + tableContent.humidity + '%'
+        document.getElementById('wind').innerHTML = 'Wind speed ' + tableContent.wind + 'km/h'
+
+        document.getElementById('content').style.display = 'none'
+        document.querySelector('.weather').style.display = 'block'
+        
+    }else{
+
+    }
+})

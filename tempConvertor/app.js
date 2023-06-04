3a3c42f3b668108469758af73b74bfc35c8d6ddc
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-04 10:45:13 +0330

    set convertor btn

--- tempConvertor/app.js
+++ tempConvertor/app.js
@@ -12,10 +12,27 @@ changeBtn.addEventListener('click', function () {
     }
 })
 
+console.log(flag)
+
 let getDecs = $.getElementById('desc')
 let resetBtn = $.getElementById('btn-reset')
-console.log(resetBtn)
 
 resetBtn.addEventListener('click', function () {
     getDecs.style.display = 'none'
+})
+
+let getInput = $.getElementById('input-degree')
+let convertorBtn = $.getElementById('btn-convert')
+// console.log(convertorBtn)
+
+convertorBtn.addEventListener('click', function () {
+    let getValue =  getInput.value
+    console.log(getValue)
+    if (flag) {
+        let showF = (1.8 * getValue) + 32
+        console.log(showF)
+    }else{
+        let showC = (getValue - 32) / 1.8
+        console.log(showC)
+    }
 })

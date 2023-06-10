973f45dd713d508fdad69fd8ad13792163a875bf
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-10 16:02:39 +0330

    update js

--- todoRawJS/todo.js
+++ todoRawJS/todo.js
@@ -1,7 +1,9 @@
 let getElemInput = document.getElementById('input-sec')
 let getElemItem = document.querySelector('.tasks')
+let getElemTask = document.querySelector('.item')
 
-console.log(getElemItem.childNodes)
+
+// console.log(getElemItem.lastElementChild)
 
 getElemInput.addEventListener('keydown', function (event) {
     
@@ -9,10 +11,15 @@ getElemInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         inputVal = getElemInput.value
         newLi = document.createElement('li')
-        
-
+            //add img & set img src
+        newImg = document.createElement('img')
+        newImg.setAttribute('src', './image/recycle-bin.png')
         getElemItem.appendChild(newLi)
         getElemItem.lastChild.textContent = inputVal
+            //add img tag to last child ul element 
+        getElemItem.lastElementChild.appendChild(newImg)
+
     }
+
     
 })

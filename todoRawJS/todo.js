41ddc3e36def24ecce8b08731d2700127316b8e6
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-10 16:04:16 +0330

    set comment

--- todoRawJS/todo.js
+++ todoRawJS/todo.js
@@ -6,20 +6,18 @@ let getElemTask = document.querySelector('.item')
 // console.log(getElemItem.lastElementChild)
 
 getElemInput.addEventListener('keydown', function (event) {
-    
-    
     if (event.key === 'Enter') {
         inputVal = getElemInput.value
         newLi = document.createElement('li')
             //add img & set img src
         newImg = document.createElement('img')
         newImg.setAttribute('src', './image/recycle-bin.png')
+            // add li tag to ul
         getElemItem.appendChild(newLi)
+            // add text value to last li
         getElemItem.lastChild.textContent = inputVal
             //add img tag to last child ul element 
         getElemItem.lastElementChild.appendChild(newImg)
 
     }
-
-    
 })

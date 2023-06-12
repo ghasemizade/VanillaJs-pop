783f577c221943471b0fc63819a4a7bfca672da2
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-12 12:26:37 +0330

    update delete icon

--- todoRawJS/todo.js
+++ todoRawJS/todo.js
@@ -1,6 +1,8 @@
 let $ = document;
 let getElemInput = $.querySelector('input')
 let getElemUl = $.querySelector('.tasks')
+let getElemLi = $.querySelectorAll('.item')
+let getIconClass = $.querySelector('.delete')
 
 function addNewTask(inputVal) {
 // add li element
@@ -13,14 +15,20 @@ function addNewTask(inputVal) {
     let newicon = $.createElement('i')
     newicon.className = 'fa fa-trash-o delete'
 
+    newicon.addEventListener('click', function (event) {
+        event.target.parentElement.remove()
+    })
+
     newLi.append(newTaskText, newicon)
     getElemUl.append(newLi)
-    console.log(newLi)
+    // console.log(newLi)
 
 }
-let inputVal = getElemInput.value.trim()
-        
+
+
+
 getElemInput.addEventListener('keydown', function (event) {
+    let inputVal = getElemInput.value.trim()
     if (event.key === 'Enter') {
         if (inputVal) {
             addNewTask(inputVal)    
@@ -29,3 +37,10 @@ getElemInput.addEventListener('keydown', function (event) {
         getElemInput.value = ''
     }
 })
+
+
+// getElemLi.forEach(function () {
+//     getIconClass.addEventListener('click', function () {
+//         getElemLi.remove()
+//     })
+// })

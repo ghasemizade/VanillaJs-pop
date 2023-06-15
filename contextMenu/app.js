e6fddcdb874899e9054e50a18d50b19017f3e1a1
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-15 22:36:45 +0330

    set keydown event

--- contextMenu/app.js
+++ contextMenu/app.js
@@ -2,7 +2,7 @@ let $ = document
 let getContextMenu = $.getElementById('context-menu')
 
 
-$.addEventListener('contextmenu', function (event) {
+$.body.addEventListener('contextmenu', function (event) {
     event.preventDefault()
     if (getContextMenu.style.display === 'none') {
         
@@ -11,9 +11,19 @@ $.addEventListener('contextmenu', function (event) {
 
         getContextMenu.style.display = 'block'
     }
-})
+    else{
+        getContextMenu.style.left = event.pageX + 'px'
+        getContextMenu.style.top = event.pageY + 'px'
+    }
+    })
 
 $.addEventListener('click', function () {
     getContextMenu.style.display = 'none'
     
+})
+
+$.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        getContextMenu.style.display = 'none'
+    }
 })

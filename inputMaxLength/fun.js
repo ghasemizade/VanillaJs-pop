5c4923428bef95329ebf30b63de9e92b6a3e6fdb
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-18 12:34:32 +0330

    set func

--- inputMaxLength/fun.js
+++ inputMaxLength/fun.js
@@ -2,4 +2,7 @@ let getInput = document.getElementById('input')
 let getCounter = document.getElementById('counter')
 
 getInput.addEventListener('keydown', function(){
+    if (getCounter.innerHTML <= 19 && getCounter.innerHTML >= 1) {
+        getCounter.innerHTML--
+    }
 })

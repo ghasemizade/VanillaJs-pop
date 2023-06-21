1b42e1736b76bf7f4770871108ac963a62586bf0
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-21 18:59:27 +0330

    set func

--- stickyNavBar/app.js
+++ stickyNavBar/app.js
@@ -1,5 +1,10 @@
 let $ = document
+let getNav =  $.getElementById('nav-bar')
 
-document.addEventListener('scroll', function (event) {
-    console.log(event)
+document.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop >= 5) {
+        getNav.classList.add('bg-black')
+    }else{
+        getNav.classList.remove('bg-black')
+    }
 })

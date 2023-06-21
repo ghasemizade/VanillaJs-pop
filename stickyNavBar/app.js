93888b755c1b1a5c38268a40c54779fb439f8530
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-21 19:04:05 +0330

    set func js

--- stickyNavBar/app.js
+++ stickyNavBar/app.js
@@ -1,10 +1,16 @@
 let $ = document
 let getNav =  $.getElementById('nav-bar')
+let getLogo = $.querySelector('.logo')
 
 document.addEventListener('scroll', function () {
     if (document.documentElement.scrollTop >= 5) {
         getNav.classList.add('bg-black')
+        getNav.classList.add('white-txt')
+        getLogo.style.fontSize = '1.7rem'
+        
     }else{
         getNav.classList.remove('bg-black')
+        getNav.classList.remove('white-txt')
+        getLogo.style.fontSize = '2.5rem'
     }
 })

00236dd028b8fc25b0a706179435e07f27913327
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-23 22:00:32 +0330

    set style to dom

---
+++ musicPlayer/music.js
@@ -0,0 +1,9 @@
+let getPlayPauseBtn = document.getElementById('play-pause')
+
+
+getPlayPauseBtn.addEventListener('click', function () {
+    let flag
+    if (flag) {
+        console.log(getPlayPauseBtn.innerHTML)
+    }
+})

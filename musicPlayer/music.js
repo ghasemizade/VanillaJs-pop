667d7515c49a509c3115baeedffd7d82a6c92546
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-23 22:03:05 +0330

    update

--- musicPlayer/music.js
+++ musicPlayer/music.js
@@ -2,8 +2,12 @@ let getPlayPauseBtn = document.getElementById('play-pause')
 
 
 getPlayPauseBtn.addEventListener('click', function () {
-    let flag
+    let flag = true
     if (flag) {
-        console.log(getPlayPauseBtn.innerHTML)
+        getPlayPauseBtn.innerHTML = '<ion-icon name="pause-outline"></ion-icon>'
+        flag = false
+    }else{
+        getPlayPauseBtn.innerHTML = '<ion-icon name="play-outline"></ion-icon>'
+        flag = true
     }
 })

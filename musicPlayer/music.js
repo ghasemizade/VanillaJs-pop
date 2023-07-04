d1f56e407dc91401c21e94387292565d454768c3
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-04 10:20:22 +0330

    update music.js

--- musicPlayer/music.js
+++ musicPlayer/music.js
@@ -1,31 +1,44 @@
 // get elemnt from dom
+let getBody = document.body
 let getPlayPauseBtn = document.getElementById('play-pause')
 let getAudio = document.querySelector('audio')
 let getNextBtn = document.getElementById('next')
 let getpreviousBtn = document.getElementById('previous')
 let getSkipBtn = document.getElementById('skip')
 let getBackBtn = document.getElementById('back')
+let getBgImg = document.getElementById('bg-poster')
 let flag = true
+
+let musicImgArray = [
+    './img-poster/Ali-Zand-Vakili-Zibatarin-Roya.jpg',
+    './img-poster/Reza-Bahram-Divaneh.jpg',
+]
 
 // set array for track src
 let musicArraySrc = [
     './media/music01.mp3',
     './media/music02.mp3',
-    // './media/LightMusic03.mp3',
-    // './media/LightMusic04.mp3',
-    // './media/LightMusic05.mp3',
 ]
 
 let audioIndex = 0
+let imgIndex = 0
 
 // add event to previous button for previous track
 getpreviousBtn.addEventListener('click', function () {
     audioIndex -= 1;
-    if (audioIndex < 0) {
-        audioIndex = 4
+    imgIndex -= 1;
+    if (audioIndex < musicArraySrc.length) {
+        audioIndex = 1
     }
+    if (imgIndex < musicImgArray.length) {
+        imgIndex = 1
+        console.log(musicImgArray[imgIndex])
+    }
+    getBgImg.setAttribute('src', musicImgArray[imgIndex])
+    getBody.style.backgroundImage = 'url' + '(' + musicImgArray[imgIndex] + ')'
     getAudio.setAttribute('src', musicArraySrc[audioIndex])
     getAudio.play()
+    getPlayPauseBtn.innerHTML = '<ion-icon name="pause-outline"></ion-icon>'
 })
 
 // add event to play and pause button for play and stop track
@@ -44,11 +57,18 @@ getPlayPauseBtn.addEventListener('click', function () {
 // add event to next button for next track
 getNextBtn.addEventListener('click', function () {
     audioIndex += 1;
+    imgIndex += 1;
     if (audioIndex > musicArraySrc.length - 1) {
         audioIndex = 0;
     }
+    if (imgIndex > musicImgArray.length - 1) {
+        imgIndex = 0
+    }
+    getBgImg.setAttribute('src', musicImgArray[imgIndex])
+    getBody.style.backgroundImage = 'url' + '(' + musicImgArray[imgIndex] + ')'
     getAudio.setAttribute('src', musicArraySrc[audioIndex])
     getAudio.play()
+    getPlayPauseBtn.innerHTML = '<ion-icon name="pause-outline"></ion-icon>'
 })
 
 // add function for -5second & +5second 

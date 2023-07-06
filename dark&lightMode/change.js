5f1a03c0d4063bbe3e1183da92da8f5fe45e1c34
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-06 22:48:53 +0330

    add js file to dom

--- dark&lightMode/change.js
+++ dark&lightMode/change.js
@@ -2,5 +2,5 @@ let getSwitchElem = document.querySelector('.switch')
 
 // add dark class to body
 getSwitchElem.addEventListener('click', function(){
-    document.body.className.add('dark')
+    document.body.classList.add('dark')
 })

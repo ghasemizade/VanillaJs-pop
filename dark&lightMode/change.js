be16ef56ecabc7160e3b059df85be11a89fdc42b
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-06 23:08:21 +0330

    add local storage

--- dark&lightMode/change.js
+++ dark&lightMode/change.js
@@ -3,4 +3,12 @@ let getSwitchElem = document.querySelector('.switch')
 // add dark class to body
 getSwitchElem.addEventListener('click', function(){
     document.body.classList.toggle('dark')
+    console.log(document.body)
+
+    if (condition) {
+        localStorage.setItem('theme', 'dark')
+    }else{
+        localStorage.setItem('theme', 'light')
+    }
+
 })

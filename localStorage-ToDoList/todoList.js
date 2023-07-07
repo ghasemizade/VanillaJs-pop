b83416f3b9569a4ef4484f35cc9a360c82e73b0e
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-07 18:24:37 +0330

    addTolist Function

--- localStorage-ToDoList/todoList.js
+++ localStorage-ToDoList/todoList.js
@@ -1,8 +1,36 @@
 const getInput = document.getElementById('input-task')
-
+const getAddBtn = document.getElementById('add')
+const getClearBtn = document.getElementById('clear')
+const getCompleteBtn = document.getElementById('complete')
+const getDeleteBtn = document.getElementById('delete')
+const getTasksContent = document.querySelector('.tasks-content')
 
 function addToList() {
-    
+    // add new div element to dom
+    let newDiv = document.createElement('div')
+    newDiv.className = 'task'
+    // add new p element to dom 
+    let newP = document.createElement('p')
+    newP.className = 'desc'
+    newP.innerHTML = getInput.value
+    // add new div task elem to dom
+    let newDivTaskBtns = document.createElement('div')
+    newDivTaskBtns.className = 'task-btns'
+    // add two buttons to task container
+    let completeBtn = document.createElement('button')
+    let deleteBtn = document.createElement('button')
+    completeBtn.id = 'complete'
+    completeBtn.innerHTML = 'complete'
+    completeBtn.style.marginRight = '5px'
+    deleteBtn.id = 'delete'
+    deleteBtn.innerHTML = 'delete'
+
+    getTasksContent.append(newDiv)
+    newDiv.append(newP)
+    newDiv.append(newDivTaskBtns)
+    newDivTaskBtns.append(completeBtn)
+    newDivTaskBtns.append(deleteBtn)
+    getInput.value = ''
 }
 function clearTodoList() {
     
@@ -10,6 +38,12 @@ function clearTodoList() {
 function completeTask() {
     
 }
-function deletetask() {
+function deleteTask() {
     
 }
+
+
+getAddBtn.addEventListener('click', addToList)
+getClearBtn.addEventListener('click', clearTodoList)
+getCompleteBtn.addEventListener('click', completeTask)
+getDeleteBtn.addEventListener('click', deleteTask)

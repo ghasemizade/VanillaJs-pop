0a55df0333be77f5be4084ab1c6872d9caf4cdc0
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-15 16:30:33 +0330

    update todolist

--- localStorage-ToDoList/todoList.js
+++ localStorage-ToDoList/todoList.js
@@ -1,83 +1,87 @@
-const getInput = document.getElementById('input-task')
-const getAddBtn = document.getElementById('add')
-const getClearBtn = document.getElementById('clear')
-const getCompleteBtn = document.getElementById('complete')
-const getDeleteBtn = document.getElementById('delete')
-const getTasksContent = document.querySelector('.tasks-content')
-const getTask = document.getElementById('task')
-const getDesc = document.querySelector('.desc')
+let $ = document
+const inputElem = $.getElementById('itemInput')
+const addButton = $.getElementById('addButton')
+const clearButton = $.getElementById('clearButton')
+const todoListElem = $.getElementById('todoList')
 
 let todosArray = []
 
-function addToList() {
+function addNewTodo () {
+    let newTodoTitle = inputElem.value
+
     let newTodoObj = {
         id: todosArray.length + 1,
-        title: getInput.value,
-        complete: false,
+        title: newTodoTitle,
+        complete: false
     }
+
+    inputElem.value = ''
 
     todosArray.push(newTodoObj)
     setLocalStorage(todosArray)
-    // add new div element to dom
-    let newDiv = document.createElement('div')
-    newDiv.className = 'task'
-    // add new p element to dom 
-    let newP = document.createElement('p')
-    newP.className = 'desc'
-    newP.innerHTML = getInput.value
-    // add new div task elem to dom
-    let newDivTaskBtns = document.createElement('div')
-    newDivTaskBtns.className = 'task-btns'
-    // add two buttons to task container
-    let completeBtn = document.createElement('button')
-    let deleteBtn = document.createElement('button')
-    completeBtn.id = 'complete'
-    completeBtn.innerHTML = 'Complete'
-    completeBtn.style.marginRight = '5px'
-    deleteBtn.id = 'delete'
-    deleteBtn.innerHTML = 'Delete'
+    todosGenerator(todosArray)
 
-    getTasksContent.append(newDiv)
-    newDiv.append(newP)
-    newDiv.append(newDivTaskBtns)
-    newDivTaskBtns.append(completeBtn)
-    newDivTaskBtns.append(deleteBtn)
-    getInput.value = ''
-}
-function clearTodoList() {
-    getTasksContent.remove()
-}
-function completeTask() {
-    getTask.style.backgroundColor = 'rgb(68, 179, 58)'
-    getTask.style.border = 'none'
-    getDesc.remove()
-    getDeleteBtn.remove()
-    getCompleteBtn.style.fontSize = '18px'
-}
-function deleteTask() {
-    getTask.style.backgroundColor = 'tomato'
-    getTask.style.border = 'none'
-    getDesc.remove()
-    getDeleteBtn.style.fontSize = '18px'
-    getCompleteBtn.remove()
-}
-function setLocalStorage(todoList) {
-    localStorage.setItem('todos', JSON.stringify(todoList))
+    inputElem.focus()
 }
 
-getAddBtn.addEventListener('click', addToList)
-getClearBtn.addEventListener('click', clearTodoList)
-getCompleteBtn.addEventListener('click', completeTask)
-getDeleteBtn.addEventListener('click', deleteTask)
+function setLocalStorage (todosList) {
+    localStorage.setItem('todos', JSON.stringify(todosList))
+}
 
-function getLocalStorage() {
+function todosGenerator (todosList) {
+
+    let newTodoLiElem, newTodoLabalElem, newTodoCompleteBtn, newTodoDeleteBtn
+
+    todoListElem.innerHTML = ''
+
+    todosList.forEach(function (todo) {
+        console.log(todo);
+        newTodoLiElem = $.createElement('li')
+        newTodoLiElem.className = 'completed well'
+
+        newTodoLabalElem = $.createElement('label')
+        newTodoLabalElem.innerHTML = todo.title
+
+        newTodoCompleteBtn = $.createElement('button')
+        newTodoCompleteBtn.className =  'btn btn-success'
+        newTodoCompleteBtn.innerHTML = 'Complete'
+        
+        newTodoDeleteBtn = $.createElement('button')
+        newTodoDeleteBtn.className = 'btn btn-danger'
+        newTodoDeleteBtn.innerHTML = 'Delete'
+
+        newTodoLiElem.append(newTodoLabalElem, newTodoCompleteBtn, newTodoDeleteBtn)
+
+        todoListElem.append(newTodoLiElem)
+    })
+}
+
+function getLocalStorage () {
     let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
-    console.log(localStorageTodos)
+
+    if (localStorageTodos) {
+        todosArray = localStorageTodos
+    } else {
+        todosArray = []
+    }
+
+    todosGenerator(todosArray)
+
 }
 
-Window.addEventListener('load', getLocalStorage)
-getInput.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        addToList()
+function clearTodos () {
+    todosArray = []
+    todosGenerator(todosArray)
+    // localStorage.clear()
+    localStorage.removeItem('todos')
+}
+
+
+window.addEventListener('load', getLocalStorage)
+addButton.addEventListener('click', addNewTodo)
+clearButton.addEventListener('click', clearTodos)
+inputElem.addEventListener('keydown', function (event) {
+    if (event.code === 'Enter') {
+        addNewTodo()
     }
 })

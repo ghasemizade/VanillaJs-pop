7afae65a96a37ebe14019b66e2eab612fc33fd03
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-17 19:13:17 +0330

    define var

---
+++ bmi/bmi.js
@@ -0,0 +1,4 @@
+const getKg = document.getElementById('kg') 
+const getCm = document.getElementById('cm')
+const getRangeKg = document.getElementById('kg-range')
+const getRangeCm = document.getElementById('cm-range')

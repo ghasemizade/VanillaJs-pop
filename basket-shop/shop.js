b1ea1f044d187307a5bf4c132838d421d8614426
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-21 21:19:53 +0330

    update js file

--- basket-shop/shop.js
+++ basket-shop/shop.js
@@ -52,10 +52,46 @@ function addProductToBasket (productId){
         return product.id === productId
     })
     userBasket.push(mainProduct)
+    basketProductGenrator(userBasket)
 }
 
 function basketProductGenrator(userBasketArray) {
     userBasketArray.forEach(product => {
         
+        let basketProductContainer = document.createElement('div')
+        basketProductContainer.classList.add('cart-row')
+
+        let basketProductDetaileContainer = document.createElement('div')
+        basketProductDetaileContainer.className = 'clart-item cart-column'
+
+        let basketProductImg = document.createElement('img')
+        basketProductImg.setAttribute('src', product.img)
+        basketProductImg.classList.add('cart-item-image')
+
+        let basketProductTitleSpan = document.createElement('span')
+        basketProductTitleSpan.classList.add('cart-item-title')
+        basketProductTitleSpan.innerHTML = product.title
+
+        basketProductDetaileContainer.append(basketProductImg, basketProductTitleSpan)
+
+        let basketProductPriceSpan = document.createElement('span')
+        basketProductPriceSpan.className = 'cart-price cart-column'
+        basketProductPriceSpan.innerHTML = product.price
+
+        let basketProductInputContainer = document.createElement('div')
+        basketProductInputContainer.className = 'cart-quantity cart-column'
+
+        let basketProductInput = document.createElement('input')
+        basketProductInput.className = 'cart-quantity-input'
+        basketProductInput.value = '1'
+        basketProductInput.setAttribute('type', 'number')
+
+        let basketProductRemoveBtn = document.createElement('button')
+        basketProductRemoveBtn.className = 'btn btn-danger'
+        basketProductRemoveBtn.innerHTML = 'Remove'
+
+        basketProductContainer.append(basketProductDetaileContainer, basketProductPriceSpan, basketProductInputContainer)
+
+        console.log(basketProductContainer)
     })
 }

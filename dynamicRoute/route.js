6e79666e0dd35b960c7b853956618e2accc8c2d1
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-29 19:25:42 +0330

    Update route.js

--- dynamicRoute/route.js
+++ dynamicRoute/route.js
@@ -1,11 +1,12 @@
 let productContainer = document.querySelector('.container')
 
 let productArray = [
-    {id: 1, title: 'Air Jordan 1 Mid SE', price: 124.95},
-    {id: 2, title: 'Air Jordan 1 Low', price: 109.95},
-    {id: 3, title: 'Air Jordan 1 Retro High OG', price: 164.95},
+    {id: 1, title: 'Air Jordan 1 Mid SE', price: 124.95, img: './imgs/air-jordan-1.jpg'},
+    {id: 2, title: 'Air Jordan 1 Low', price: 109.95, img: './imgs/air-jordan-2.jpg'},
+    {id: 3, title: 'Air Jordan 1 Retro High OG', price: 164.95, img: './imgs/air-jordan-3.jpg'},
 ]
 
 productArray.forEach(function (product) {
-    productContainer.insertAdjacentElement('beforeend', '<div class="item"><div class="details"><img src="./imgs/air-jordan-1.jpg" alt="jordan-1"><h3>Air Jordan 1 Mid SE</h3><p class="desc">Mens Shoes</p><p class="price">£124.95</p><a href="">SEE MORE</a></div></div>')
+    productContainer.insertAdjacentElement('beforeend', '<div class="item"><div class="details"><img src="'+ productArray.img +'" alt="jordan-1"><h3>' + productArray.title + '</h3><p class="desc">Mens Shoes</p><p class="price">£' + productArray.price + '</p><a href="">SEE MORE</a></div></div>')
+    
 })

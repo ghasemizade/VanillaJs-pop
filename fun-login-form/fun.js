8bdc044f320007d6d02cdf3d70349e9bc8315c0f
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2023-12-04 08:21:29 +0330

    update

--- fun-login-form/fun.js
+++ fun-login-form/fun.js
@@ -1,5 +1,7 @@
 const userInputElem = document.getElementById("username")
 const passInputElem = document.getElementById("password")
+const leftEye = document.querySelector(".righteye")
+const rightEye = document.querySelector(".lefteye")
 
 inputElem.addEventListener('focus', () => {
     console.log("focus");

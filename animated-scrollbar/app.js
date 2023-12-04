6b034a0299df14fd37567fd02db70898d070d087
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2023-12-04 20:39:01 +0330

    update

--- animated-scrollbar/app.js
+++ animated-scrollbar/app.js
@@ -1,14 +1,26 @@
 const counters = document.querySelectorAll('.num')
 let benefitsContainer = document.querySelector('.benefits')
+let startCounter = false
 
 window.addEventListener('scroll', () => {
-    console.log(window.scrollY, benefitsContainer.offsetTop);
+    // console.log(window.scrollY, benefitsContainer.offsetTop);
 
     if (window.scrollY >= benefitsContainer.offsetTop) {
-        counters.forEach(counter => setCounter(counter))
+        if (!startCounter) {
+            counters.forEach(counter => setCounter(counter))
+        }
+        startCounter = true
     }
 })
 
 function setCounter(element) {
-    console.log(element);
+    let elemNumCount = element.dataset.counter
+
+    let counterInterval = setInterval(() => {
+        if(element.textContent == elemNumCount) {
+            clearInterval(counterInterval)
+        }
+
+        element.textContent++
+    }, 5)
 }

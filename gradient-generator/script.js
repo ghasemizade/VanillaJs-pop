39705ddc37f70fee914b7c994d06d1138dd9de9d
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-01-22 18:56:35 +0330

    date set update

--- gradient-generator/script.js
+++ gradient-generator/script.js
@@ -1,9 +1,21 @@
-let $ = document
-let dirButton = $.querySelectorAll("button")
+const $ = document
+const dirButton = $.querySelectorAll("button");
+const firstColorInput = $.querySelector("#color-a")
+const secondColorInput = $.querySelector("#color-b")
+const generateBtn = $.getElementById("#submit")
+const codeTextArea = $.getElementById("#code")
+const copyBtn = $.getElementById("#copy")
+let select;
 
 dirButton.forEach(item => {
     item.addEventListener("click", () =>{
-        item.classList = "active"
+        select = false
+        if (!select) {
+            item.className = "active";
+        }else{
+            item.classList.remove("active")            
+            select = false
+        }
     })
 })
 

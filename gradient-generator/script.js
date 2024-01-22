e3c724e634f508fb920509614a296b5176472e00
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-01-22 19:50:24 +0330

    last update gradient

--- gradient-generator/script.js
+++ gradient-generator/script.js
@@ -1,22 +1,51 @@
 const $ = document
-const dirButton = $.querySelectorAll("button");
+const dirButton = $.querySelectorAll(".buttons button");
 const firstColorInput = $.querySelector("#color-a")
 const secondColorInput = $.querySelector("#color-b")
-const generateBtn = $.getElementById("#submit")
-const codeTextArea = $.getElementById("#code")
-const copyBtn = $.getElementById("#copy")
-let select;
+const generateBtn = $.querySelector("#submit")
+const codeTextArea = $.querySelector("#code")
+const copyBtn = $.querySelector("#copy")
 
-dirButton.forEach(item => {
-    item.addEventListener("click", () =>{
-        select = false
-        if (!select) {
-            item.className = "active";
-        }else{
-            item.classList.remove("active")            
-            select = false
-        }
+let currentDirection = "to bottom";
+
+const generateCssCode = () => {
+    let cssCode = `background: linear-gradient(${currentDirection}, ${firstColorInput.value}, ${secondColorInput.value});`
+
+    codeTextArea.value = cssCode;
+    $.body.style.cssText = cssCode;
+}
+
+const setDirection = (direction, directionElem) => {
+    for(let item of dirButton){
+        item.classList.remove('active')
+    }
+
+    if (directionElem.tagName === 'BUTTON') {
+        directionElem.classList.add('active')
+    } else {
+        directionElem.parentElement.classList.add('active')
+    }
+
+    currentDirection = direction;
+}
+
+
+dirButton.forEach(direction => {
+    direction.addEventListener('click', (event) =>{
+        let direction = event.target.dataset.direction;
+        
+        setDirection(direction, event.target);
     })
 })
 
-console.log(dirButton)
+copyCssCode = () =>{
+    codeTextArea.select()
+    document.execCommand('copy')
+
+    alert("Css Code Copied")
+}
+
+generateBtn.addEventListener('click', generateCssCode)
+copyBtn.addEventListener('click', copyCssCode)
+
+generateCssCode()
